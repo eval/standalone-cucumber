3278da061bbec9aefc3dc973d34d3c1bcdf724e5
Gert Goet <gert@thinkcreate.nl>
2012-11-08 11:05:51 +0100

    Update README

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "http://rubygems.org"
 gem 'cucumber'
 gem 'capybara-webkit'
 gem 'rspec'
+gem 'rake'
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,9 @@
-begin require 'rspec/expectations'; rescue LoadError; require 'spec/expectations'; end
-require 'capybara' 
-require 'capybara/dsl' 
+require 'rspec/expectations'
+require 'capybara'
+require 'capybara/dsl'
 require 'capybara/cucumber'
-require 'capybara-webkit' # I added this
+require 'capybara-webkit'
 Capybara.default_driver = :webkit
 Capybara.app_host = 'http://github.com'
 Capybara.run_server = false
-World(Capybara) 
+World(Capybara)
